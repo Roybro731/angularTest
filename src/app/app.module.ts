7aaeba648264708c31f5c4c65da2b24e5bd0ce3f
brody <brody@microfocus.com>
2018-09-02 18:07:59 +0300

    adding routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
+import { AppRouteModuleModule } from './app-route-module/app-route-module.module';
+import { ToDoModuleModule } from './to-do-module/to-do-module.module';
 import { AppComponent } from './app.component';
 import { MainTableComponent } from './main-table/main-table.component';
 import { HeaderComponent } from './header/header.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { reducer } from './store/proucts.reducer';
+import { ErrorPageComponent } from './sharedComponents/error-page/error-page.component';
 
 @NgModule({
   declarations: [
@@ -15,9 +19,13 @@ import { reducer } from './store/proucts.reducer';
     MainTableComponent,
     HeaderComponent,
     AddProductComponent,
+    ErrorPageComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule,
+    ToDoModuleModule,
+    AppRouteModuleModule,
     StoreModule.forRoot({
       products: reducer
     }),
